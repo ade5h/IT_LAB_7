6c7ba8bb3fd49c763012ddc65d3d1cf9f4713303
Adesh Attavar <adesh.attavar@gmail.com>
2021-06-03 18:26:20 +0530

    endsemFinal

--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponseRedirect
 from .models import Student
 
 def search(request):
-	print(request.GET.id)
-	redirect('allStudents/'+id)
-		
+	id = request.GET.getlist("id")
+	url = 'allStudents/'+id[0]
+	return HttpResponseRedirect(url)
 
 def getStudent(request, id):
 	student = Student.objects.get(regnum=id)
